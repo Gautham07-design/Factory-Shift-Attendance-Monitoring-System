dfe227bdc9766fda7d8000cc6aecd52f913b8115
Gautham07-design <singaravelusgauthaman@gmail.com>
2025-12-27 13:07:09 +0530

    Add files via upload

---
+++ FactoryShiftAttendanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.FactoryShiftAttendanceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryShiftAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryShiftAttendanceSystemApplication.class, args);
+	}
+
+}
